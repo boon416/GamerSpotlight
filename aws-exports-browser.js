81790073e3be094692722792576bc9ddef54df98
boon416 <136696034+virtucard1506@users.noreply.github.com>
2025-04-16 13:25:48 +0800

    update on sigh up

---
+++ aws-exports-browser.js
@@ -0,0 +1,21 @@
+window.aws_exports = {
+    "aws_project_region": "ap-southeast-1",
+    "aws_cognito_identity_pool_id": "ap-southeast-1:98591ccc-b3de-43e2-a4dd-530bffb633ba",
+    "aws_cognito_region": "ap-southeast-1",
+    "aws_user_pools_id": "ap-southeast-1_uVcvEfm7k",
+    "aws_user_pools_web_client_id": "3odjsbbmddhe2d0gl633ohcttb",
+    "oauth": {},
+    "aws_cognito_username_attributes": ["EMAIL"],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": ["EMAIL"],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": ["SMS"],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": ["EMAIL"],
+    "aws_content_delivery_bucket": "amerpotlight-20250416103317-hostingbucket-dev",
+    "aws_content_delivery_bucket_region": "ap-southeast-1",
+    "aws_content_delivery_url": "https://d1fcigfg56hpkr.cloudfront.net"
+};
