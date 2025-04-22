776604fcc4eb83c2246f41e31e8783abefa74c68
boon416 <136696034+virtucard1506@users.noreply.github.com>
2025-04-22 17:05:02 +0800

    update index.html and mai.js

--- js/main.js
+++ js/main.js
@@ -136,5 +136,20 @@ $(".slider-two")
     });
   });
   
-
   import { UserManager } from 'https://cdn.skypack.dev/oidc-client-ts';
+
+const cognitoAuthConfig = {
+  authority: "https://ap-southeast-1ypgkwwghv.auth.ap-southeast-1.amazoncognito.com",
+  client_id: "p36ba0bp3u8sqr55fc682n714",
+  redirect_uri: "https://staging.d3744hmwully3z.amplifyapp.com/index.html",
+  response_type: "code",
+  scope: "openid email profile"
+};
+
+export const userManager = new UserManager({ ...cognitoAuthConfig });
+
+export async function signOutRedirect () {
+  const logoutUri = cognitoAuthConfig.redirect_uri;
+  const cognitoDomain = cognitoAuthConfig.authority;
+  window.location.href = `${cognitoDomain}/logout?client_id=${cognitoAuthConfig.client_id}&logout_uri=${encodeURIComponent(logoutUri)}`;
+}
